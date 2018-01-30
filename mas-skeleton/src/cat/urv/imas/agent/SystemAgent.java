60150637747ae38b3547fc184237bbac306a10c8
Montroigenc <aleixtoda@gmail.com>
2018-01-30 23:07:00 +0100

    Aixo si k va

--- mas-skeleton/src/cat/urv/imas/agent/SystemAgent.java
+++ mas-skeleton/src/cat/urv/imas/agent/SystemAgent.java
@@ -522,7 +522,7 @@ public class SystemAgent extends ImasAgent {
 
             //6. Substitute the old map with the new checked map
             currentMap = nextTurnMap.clone();            
-            Thread.sleep(10);
+            //Thread.sleep(10);
             result = true;
         
         } catch (Exception ex) {
